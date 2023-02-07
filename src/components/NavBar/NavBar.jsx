c01562277edc6a45abc1539d28ab4672fb2bc554
Chris Mems <christopher.mems@.com>
2023-02-07 17:16:34 -0600

    style navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,21 +1,26 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import Logo from '../../assets/branding/logo.png'
 
 const NavBar = ({ user, handleLogout }) => {
+  
+  const publicLinks = (
+    <ul className='m-0 w-full flex list-none font-bold items-center justify-end pl-0'>
+      <li className='ml-8 sm:text-base'><NavLink to='/login'>LOG IN</NavLink></li>
+      <li className='ml-8 sm:text-base'><NavLink to='/signup'>SIGN UP</NavLink></li>
+    </ul>
+  )
+
+  const protectLinks = (
+    <ul className='m-0 w-full flex list-none font-bold items-center justify-end pl-0'>
+      <li className='ml-8 sm:text-base'><NavLink to='/logout' onClick={handleLogout}>LOG OUT</NavLink></li>
+    </ul>
+  )
+
+
   return (
-    <nav>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><Link to="/profiles">Profiles</Link></li>
-          <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
-          <li><Link to="/change-password">Change Password</Link></li>
-        </ul>
-      :
-        <ul>
-          <li><Link to="/login">Log In</Link></li>
-          <li><Link to="/signup">Sign Up</Link></li>
-        </ul>
-      }
+    <nav className='top-0 z-0 w-full h-20 flex fixed py-3.5 px-6 items-center bg-white border-b border-[rgb(200,200,200)]'>
+      <NavLink className='w-16' to={'/'}><img src={Logo}  alt='A plane over the ocean' /></NavLink>
+      {user ? protectLinks : publicLinks}
     </nav>
   )
 }
