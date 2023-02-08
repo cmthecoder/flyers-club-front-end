e62e6c71be74338222cf3493aae84b9db5cae42b
Chris Mems <christopher.mems@.com>
2023-02-07 19:53:24 -0600

    create icon component

--- src/components/AuthorInfo/AuthorInfo.jsx
+++ src/components/AuthorInfo/AuthorInfo.jsx
@@ -9,10 +9,10 @@ const AuthorInfo = ({ content }) => {
     const photo = content.author.photo ? content.author.photo : ProfileIcon
 
   return (
-    <div>
-      <img src={photo} alt="The user's avatar" />
-      <section>
-        <h4>{content.author.name}</h4>
+    <div className='m-0 flex items-center justify-start'>
+      <img className='w-8 mr-3 rounded-full' src={photo} alt="The user's avatar" />
+      <section className='flex flex-col'>
+        <h4 className='opacity-75'>{content.author.name}</h4>
         <DateCard createdAt={content.createdAt} />
       </section>
     </div>
---
+++ src/components/Icons/Icons.jsx
@@ -0,0 +1,37 @@
+import Add from '../../assets/icons/add.png'
+import FAA from '../../assets/icons/faa.png'
+import Edit from '../../assets/icons/edit.png'
+import News from '../../assets/icons/news.svg'
+import Like from '../../assets/icons/like.png'
+import Event from '../../assets/icons/event.png'
+import Liked from '../../assets/icons/liked.png'
+import Trash from '../../assets/icons/trash.png'
+import Advice from '../../assets/icons/games.png'
+import Create from '../../assets/icons/create.svg'
+import Milestone from '../../assets/icons/movies.png'
+import Comments from '../../assets/icons/comments.png'
+import Calendar from '../../assets/icons/calendar.svg'
+import Adventure from '../../assets/icons/adventure.png'
+
+const Icon = ({ category }) => {
+  const icons = {
+    Add: Add,
+    FAA: FAA,
+    News: News,
+    Like: Like,
+    Edit: Edit,
+    Liked: Liked,
+    Trash: Trash,
+    Event: Event,
+    Advice: Advice,
+    Create: Create,
+    Calendar: Calendar,
+    Comments: Comments,
+    Milestone: Milestone,
+    Adventure: Adventure,
+  }
+
+  return <img className="icon" src={icons[category]} alt={`A ${category} icon.`} />
+}
+
+export default Icon
